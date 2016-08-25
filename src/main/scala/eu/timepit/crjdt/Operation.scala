63de2b9b1393e12f3c05f1515cdec748b76d0c32
Frank S. Thomas <frank@timepit.eu>
2016-08-25 05:50:01 +0200

    Stub Operation

--- src/main/scala/eu/timepit/crjdt/Operation.scala
+++ src/main/scala/eu/timepit/crjdt/Operation.scala
@@ -1,3 +1,14 @@
 package eu.timepit.crjdt
 
-class Operation {}
+import eu.timepit.crjdt.Operation.Mutation
+
+final case class Operation(id: Id, deps: Set[Id], cur: Cursor, mut: Mutation)
+
+object Operation {
+  sealed trait Mutation extends Product with Serializable
+  object Mutation {
+    final case class Assign(v: Val) extends Mutation
+    final case class Insert(v: Val) extends Mutation
+    case object Delete extends Mutation
+  }
+}
