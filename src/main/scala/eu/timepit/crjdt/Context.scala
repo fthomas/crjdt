91b465bbce84e3fbc3b0b3ba8b4f3b40cad7a2a0
Frank S. Thomas <frank@timepit.eu>
2016-08-27 06:32:01 +0200

    More plugins

--- src/main/scala/eu/timepit/crjdt/Context.scala
+++ src/main/scala/eu/timepit/crjdt/Context.scala
@@ -13,7 +13,7 @@ object Context {
 
   case object Empty extends Context
   final case class Leaf(value: Value) extends Context
-  final case class Assoc(key: Tagged, ctx: Context) extends Context
+  final case class Assoc(key: Tagged, ctx: Context, pres: Any) extends Context
   final case class Many(assocs: List[Assoc]) extends Context
 
   // Is this the same as Cursor.Key?
