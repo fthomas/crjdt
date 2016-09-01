69ad3eda6d589a256b6b0a87b5c8b25f54f4ee39
Frank S. Thomas <frank@timepit.eu>
2016-09-01 06:30:23 +0200

    Add Arbitrary[Key] instance

--- src/test/scala/eu/timepit/crjdt/testInstances.scala
+++ src/test/scala/eu/timepit/crjdt/testInstances.scala
@@ -1,6 +1,7 @@
 package eu.timepit.crjdt
 
-import org.scalacheck.Arbitrary
+import eu.timepit.crjdt.Key.{DocK, HeadK, IdK, StrK}
+import org.scalacheck.{Arbitrary, Gen}
 
 object testInstances {
   implicit val idArbitrary: Arbitrary[Id] = {
@@ -9,5 +10,13 @@ object testInstances {
       p <- Arbitrary.arbitrary[String]
     } yield Id(c, p)
     Arbitrary(gen)
+  }
+
+  implicit val keyArbitrary: Arbitrary[Key] = {
+    val doc = Gen.const(DocK)
+    val head = Gen.const(HeadK)
+    val id = Arbitrary.arbitrary[Id].map(IdK.apply)
+    val str = Arbitrary.arbitrary[String].map(StrK.apply)
+    Arbitrary(Gen.oneOf(doc, head, id, str))
   }
 }
