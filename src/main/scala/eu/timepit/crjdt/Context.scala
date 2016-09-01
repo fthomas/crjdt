4c6a8e7ccff3aa321cce1bc7d05b9de9c1b13b59
Frank S. Thomas <frank@timepit.eu>
2016-09-01 06:19:28 +0200

    Working on Context

--- src/main/scala/eu/timepit/crjdt/Context.scala
+++ src/main/scala/eu/timepit/crjdt/Context.scala
@@ -1,15 +1,55 @@
 package eu.timepit.crjdt
 
-import eu.timepit.crjdt.Context.{ListCtx, MapCtx, RegCtx}
+import eu.timepit.crjdt.Context._
 import eu.timepit.crjdt.Operation.Mutation
-import eu.timepit.crjdt.Operation.Mutation.DeleteM
+import eu.timepit.crjdt.Operation.Mutation.{AssignM, DeleteM, InsertM}
 import eu.timepit.crjdt.Tag.{ListT, MapT, RegT}
+import eu.timepit.crjdt.Val.{EmptyList, EmptyMap}
 
 sealed trait Context extends Product with Serializable {
   def key: Tag
 
   // PRESENCE1, PRESENCE2
   def pres: Set[Id]
+
+  def applyOp(op: Operation): Context =
+    op.cur match {
+      case Cursor(Vector(), k) =>
+        op.mut match {
+          // EMPTY-MAP
+          case AssignM(EmptyMap) => ???
+
+          // EMPTY-LIST
+          case AssignM(EmptyList) => ???
+
+          // ASSIGN
+          case AssignM(v) => ???
+
+          // INSERT1, INSERT2
+          case InsertM(v) => ???
+
+          // DELETE
+          case DeleteM => ???
+        }
+
+      // DESCEND
+      case Cursor(k +: _, _) =>
+        val child = child2(k)
+        val cur2 = op.cur.dropFirst
+        val op2 = op.copy(cur = cur2)
+        val childp = child.applyOp(op2)
+        val ctxp = addId(k, op.id, op.mut)
+        val assoc = AssocCtx(k, childp, Set.empty)
+        // TODO: ctxp [assoc], i.e. add assoc to ctxp
+        ???
+    }
+
+  def addAssoc(assoc: AssocCtx): Context =
+    this match {
+      case MapCtx(k, p, c) => ???
+      case ListCtx(_, _, _) => ???
+      case _ => ???
+    }
 
   def addId(key: Tag, id: Id, mut: Mutation): Context =
     mut match {
@@ -20,30 +60,48 @@ sealed trait Context extends Product with Serializable {
       case _ => if (key == this.key) withPres(pres + id) else this
     }
 
-  /*
   def getChild(key: Tag): Option[Context] =
     this match {
-      case MapCtx(k, _, children) if k == key => ??? // children
-      case ListCtx(k,_, children) if k == key => ??? // children
-        // in lists we have to perform a linear search for key
-        // since the cursor just contains the Id of the element
-        // (or HeadK)
-      case RegCtx(k,_, _) => ???
+      case AssocCtx(k, ctx, _) if k == key => Some(ctx)
+      case MapCtx(_, _, children) => children.get(key)
+      case ListCtx(_, _, children) => ??? // children
+      // in lists we have to perform a linear search for key
+      // since the cursor just contains the Id of the element
+      // (or HeadK)
+      case RegCtx(k, _, _) => None
     }
-  */
+
+  def child2(key: Tag): Context =
+    getChild(key) match {
+      case Some(x) => x
+      case None =>
+        key match {
+          // CHILD-MAP
+          case k @ MapT(_) => MapCtx(k, Set.empty, Map.empty)
+          // CHILD-LIST
+          case k @ ListT(_) => ListCtx(k, Set.empty, CtxList.empty)
+          // CHILD-REG
+          case k @ RegT(_) => RegCtx(k, Set.empty, Map.empty)
+        }
+    }
 
   def withPres(pres: Set[Id]): Context =
     this match {
       case ctx: MapCtx => ctx.copy(pres = pres)
       case ctx: ListCtx => ctx.copy(pres = pres)
       case ctx: RegCtx => ctx.copy(pres = pres)
+      case ctx: AssocCtx => ctx.copy(pres = pres)
     }
 }
 
 object Context {
+  // ctx must not be AssocCtx
+  final case class AssocCtx(key: Tag, ctx: Context, pres: Set[Id])
+      extends Context
+
   final case class MapCtx(key: MapT,
                           pres: Set[Id],
-                          children: Map[Tag, Context])
+                          children: Map[Tag, AssocCtx])
       extends Context
 
   final case class ListCtx(key: ListT, pres: Set[Id], children: CtxList)
@@ -58,7 +116,7 @@ object Context {
 
   object CtxList {
     final case class Head(next: CtxList) extends CtxList
-    final case class Node(ctx: Context, next: CtxList) extends CtxList
+    final case class Node(ctx: AssocCtx, next: CtxList) extends CtxList
     case object Tail extends CtxList
 
     def empty: CtxList =
