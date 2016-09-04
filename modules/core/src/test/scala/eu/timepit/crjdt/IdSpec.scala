55766c0643563eecbd5635da34fff3585ddae6ca
Frank S. Thomas <frank@timepit.eu>
2016-09-04 22:21:09 +0200

    Use objects for ScalaCheck properties consistently

--- modules/core/src/test/scala/eu/timepit/crjdt/IdSpec.scala
+++ modules/core/src/test/scala/eu/timepit/crjdt/IdSpec.scala
@@ -4,6 +4,6 @@ import cats.kernel.laws.OrderLaws
 import eu.timepit.crjdt.arbitrary._
 import org.scalacheck.Properties
 
-class IdSpec extends Properties("Id") {
+object IdSpec extends Properties("Id") {
   include(OrderLaws[Id].order.all)
 }
