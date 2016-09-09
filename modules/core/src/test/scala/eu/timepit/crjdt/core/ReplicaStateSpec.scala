a61f6c7f13b6f9039aad86a4aadca002f0fd09dd
Frank S. Thomas <frank@timepit.eu>
2016-09-09 21:45:49 +0200

    Check convergence of three replica

--- modules/core/src/test/scala/eu/timepit/crjdt/core/ReplicaStateSpec.scala
+++ modules/core/src/test/scala/eu/timepit/crjdt/core/ReplicaStateSpec.scala
@@ -23,6 +23,19 @@ object ReplicaStateSpec extends Properties("ReplicaState") {
     p1.context ?= q1.context
   }
 
+  property("convergence 3") = forAll {
+    (cmds1: List[Cmd], cmds2: List[Cmd], cmds3: List[Cmd]) =>
+      val p0 = ReplicaState.empty("p").applyCmds(cmds1)
+      val q0 = ReplicaState.empty("q").applyCmds(cmds2)
+      val r0 = ReplicaState.empty("r").applyCmds(cmds3)
+
+      val p1 = p0.applyRemoteOps(q0.generatedOps ++ r0.generatedOps)
+      val q1 = q0.applyRemoteOps(p0.generatedOps ++ r0.generatedOps)
+      val r1 = r0.applyRemoteOps(p0.generatedOps ++ q0.generatedOps)
+
+      (p1.context ?= q1.context) && (q1.context ?= r1.context)
+  }
+
   property("commutativity") = forAll { (cmds: List[Cmd]) =>
     val p = ReplicaState.empty("p").applyCmds(cmds)
     val q = ReplicaState.empty("q")
