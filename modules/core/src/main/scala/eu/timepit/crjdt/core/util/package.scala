ca10c5a94d2404ac745d28b034dc0eb07bb8ebf0
Frank S. Thomas <frank@timepit.eu>
2016-09-11 18:10:35 +0200

    Document removeOrUpdate

--- modules/core/src/main/scala/eu/timepit/crjdt/core/util/package.scala
+++ modules/core/src/main/scala/eu/timepit/crjdt/core/util/package.scala
@@ -3,6 +3,10 @@ package eu.timepit.crjdt.core
 import cats.Foldable
 
 package object util {
+
+  /** Removes `key` from `map` if `value` is empty or else adds the
+    * key-value pair to `map`.
+    */
   final def removeOrUpdate[K, F[_], A](map: Map[K, F[A]], key: K, value: F[A])(
       implicit F: Foldable[F]): Map[K, F[A]] =
     if (F.isEmpty(value)) map - key else map.updated(key, value)
