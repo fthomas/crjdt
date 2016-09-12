d68f75617efffbf2f7c2c1b7a95b45bf3e700e00
Frank S. Thomas <frank@timepit.eu>
2016-09-12 23:57:41 +0200

    Document Expr

--- modules/core/src/main/scala/eu/timepit/crjdt/core/Expr.scala
+++ modules/core/src/main/scala/eu/timepit/crjdt/core/Expr.scala
@@ -1,5 +1,13 @@
 package eu.timepit.crjdt.core
 
+/** This corresponds to the expression construct `EXPR` in Kleppmann
+  * and Beresford. It is used to construct a `[[Cursor]]`.
+  *
+  * In contrast to the paper, `Expr` does not include the `keys` and
+  * `values` operations which can be used to query the state of a
+  * document. This simplifies the evaluation of an `Expr` which always
+  * yields a `[[Cursor]]`.
+  */
 sealed trait Expr extends Product with Serializable
 
 object Expr {
