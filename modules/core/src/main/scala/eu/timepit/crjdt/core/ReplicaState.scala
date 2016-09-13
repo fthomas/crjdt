e1821a86aa1224ac9155fe5b0c8c42ed72813cad
Frank S. Thomas <frank@timepit.eu>
2016-09-13 15:18:02 +0200

    Rename applyExpr to evalExpr

--- modules/core/src/main/scala/eu/timepit/crjdt/core/ReplicaState.scala
+++ modules/core/src/main/scala/eu/timepit/crjdt/core/ReplicaState.scala
@@ -19,17 +19,17 @@ final case class ReplicaState(replicaId: ReplicaId,
   def applyCmd(cmd: Cmd): ReplicaState =
     cmd match {
       case Let(x, expr) => // LET
-        val cur = applyExpr(expr)
+        val cur = evalExpr(expr)
         copy(variables = variables.updated(x, cur))
 
       case Assign(expr, value) => // MAKE-ASSIGN
-        makeOp(applyExpr(expr), AssignM(value))
+        makeOp(evalExpr(expr), AssignM(value))
 
       case Insert(expr, value) => // MAKE-INSERT
-        makeOp(applyExpr(expr), InsertM(value))
+        makeOp(evalExpr(expr), InsertM(value))
 
       case Delete(expr) => // MAKE-DELETE
-        makeOp(applyExpr(expr), DeleteM)
+        makeOp(evalExpr(expr), DeleteM)
 
       case Sequence(cmd1, cmd2) => // EXEC
         applyCmd(cmd1).applyCmd(cmd2)
@@ -40,37 +40,6 @@ final case class ReplicaState(replicaId: ReplicaId,
     cmds match {
       case x :: xs => applyCmd(x).applyCmds(xs)
       case Nil => this
-    }
-
-  def applyExpr(expr: Expr): Cursor =
-    expr match {
-      case Doc => // DOC
-        Cursor.doc
-
-      case v @ Var(_) => // VAR
-        variables.get(v) match {
-          case Some(cur) => cur
-          // This case violates VAR's precondition x elem dom(A_p).
-          case None => Cursor.doc
-        }
-
-      case DownField(expr2, key) => // GET
-        val cur = applyExpr(expr2)
-        cur.finalKey match {
-          // This case violates GET's precondition k_n != head.
-          // It corresponds to the dubious EXPR `iter[key]` which should
-          // be impossible to construct with the EXPR API.
-          case HeadK => cur
-          case _ => cur.append(MapT.apply, StrK(key))
-        }
-
-      case Iter(expr2) => // ITER
-        val cur = applyExpr(expr2)
-        cur.append(ListT.apply, HeadK)
-
-      case Next(expr2) => // NEXT1
-        val cur = applyExpr(expr2)
-        context.next(cur)
     }
 
   // APPLY-LOCAL
@@ -96,6 +65,37 @@ final case class ReplicaState(replicaId: ReplicaId,
 
   def currentId: Id =
     Id(opsCounter, replicaId)
+
+  def evalExpr(expr: Expr): Cursor =
+    expr match {
+      case Doc => // DOC
+        Cursor.doc
+
+      case v @ Var(_) => // VAR
+        variables.get(v) match {
+          case Some(cur) => cur
+          // This case violates VAR's precondition x elem dom(A_p).
+          case None => Cursor.doc
+        }
+
+      case DownField(expr2, key) => // GET
+        val cur = evalExpr(expr2)
+        cur.finalKey match {
+          // This case violates GET's precondition k_n != head.
+          // It corresponds to the dubious EXPR `iter[key]` which should
+          // be impossible to construct with the EXPR API.
+          case HeadK => cur
+          case _ => cur.append(MapT.apply, StrK(key))
+        }
+
+      case Iter(expr2) => // ITER
+        val cur = evalExpr(expr2)
+        cur.append(ListT.apply, HeadK)
+
+      case Next(expr2) => // NEXT1
+        val cur = evalExpr(expr2)
+        context.next(cur)
+    }
 
   def findApplicableRemoteOp: Option[Operation] =
     receivedOps.find { op =>
--- modules/core/src/test/scala/eu/timepit/crjdt/core/EvalExprSpec.scala
+++ modules/core/src/test/scala/eu/timepit/crjdt/core/EvalExprSpec.scala
@@ -6,35 +6,35 @@ import eu.timepit.crjdt.core.syntax._
 import org.scalacheck.Prop._
 import org.scalacheck.Properties
 
-object ApplyExprSpec extends Properties("ReplicaState.applyExpr") {
+object EvalExprSpec extends Properties("ReplicaState.evalExpr") {
   val state = ReplicaState.empty("p")
 
   property("doc") = secure {
-    state.applyExpr(doc) ?= Cursor.withFinalKey(DocK)
+    state.evalExpr(doc) ?= Cursor.withFinalKey(DocK)
   }
 
   property("""doc["key"]""") = secure {
-    state.applyExpr(doc.downField("key")) ?=
+    state.evalExpr(doc.downField("key")) ?=
       Cursor(Vector(MapT(DocK)), StrK("key"))
   }
 
   property("""doc["key"].iter""") = secure {
-    state.applyExpr(doc.downField("key").iter) ?=
+    state.evalExpr(doc.downField("key").iter) ?=
       Cursor(Vector(MapT(DocK), ListT(StrK("key"))), HeadK)
   }
 
   property("doc.iter") = secure {
-    state.applyExpr(doc.iter) ?= Cursor(Vector(ListT(DocK)), HeadK)
+    state.evalExpr(doc.iter) ?= Cursor(Vector(ListT(DocK)), HeadK)
   }
 
   property("doc.iter.next") = secure {
-    state.applyExpr(doc.iter.next) ?= Cursor(Vector(ListT(DocK)), HeadK)
+    state.evalExpr(doc.iter.next) ?= Cursor(Vector(ListT(DocK)), HeadK)
   }
 
   property("stack safety") = secure {
     val expr = Iterator.iterate(doc)(_.downField("k")).drop(20000).next()
     // println(expr)
-    // val cur = ReplicaState.empty("").applyExpr(expr)
+    // val cur = ReplicaState.empty("").evalExpr(expr)
     // println(cur)
     true
   }
