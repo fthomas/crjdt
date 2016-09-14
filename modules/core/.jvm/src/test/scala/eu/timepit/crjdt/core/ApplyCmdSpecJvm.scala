b66dc56b3a80889d4ef72b8b68384e356016bb20
Frank S. Thomas <frank@timepit.eu>
2016-09-14 21:32:31 +0200

    Add (disabled) stack safety test for applyCmd

---
+++ modules/core/.jvm/src/test/scala/eu/timepit/crjdt/core/ApplyCmdSpecJvm.scala
@@ -0,0 +1,16 @@
+package eu.timepit.crjdt.core
+
+import eu.timepit.crjdt.core.syntax._
+import org.scalacheck.Prop._
+import org.scalacheck.Properties
+
+class ApplyCmdSpecJvm extends Properties("ReplicaState.applyCmd") {
+  val state = ReplicaState.empty("p")
+
+  property("stack safety") = secure {
+    val cmd = doc := `{}`
+    val many = Iterator.iterate(cmd)(_ `;` cmd).drop(10000).next()
+    //state.applyCmd(many)
+    true
+  }
+}
