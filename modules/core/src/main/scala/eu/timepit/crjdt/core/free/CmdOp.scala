7f7458b4d2183bbf6949b51823fa0e939167ff5b
Frank S. Thomas <frank@timepit.eu>
2016-10-07 23:09:30 +0200

    Add type

--- modules/core/src/main/scala/eu/timepit/crjdt/core/free/CmdOp.scala
+++ modules/core/src/main/scala/eu/timepit/crjdt/core/free/CmdOp.scala
@@ -25,43 +25,44 @@ object CmdOp {
 
   type ReplicaState[A] = State[Replica, A]
 
-  val cmdOpToReplicaState = new FunctionK[CmdOp, ReplicaState] {
-    override def apply[A](op: CmdOp[A]): ReplicaState[A] =
-      op match {
-        // MAKE-ASSIGN
-        case Assign(cur, value) =>
-          State.modify(_.makeOp(cur, AssignM(value)))
+  val cmdOpToReplicaState: FunctionK[CmdOp, ReplicaState] =
+    new FunctionK[CmdOp, ReplicaState] {
+      override def apply[A](op: CmdOp[A]): ReplicaState[A] =
+        op match {
+          // MAKE-ASSIGN
+          case Assign(cur, value) =>
+            State.modify(_.makeOp(cur, AssignM(value)))
 
-        // MAKE-INSERT
-        case Insert(cur, value) =>
-          State.modify(_.makeOp(cur, InsertM(value)))
+          // MAKE-INSERT
+          case Insert(cur, value) =>
+            State.modify(_.makeOp(cur, InsertM(value)))
 
-        // MAKE-DELETE
-        case Delete(cur) =>
-          State.modify(_.makeOp(cur, DeleteM))
+          // MAKE-DELETE
+          case Delete(cur) =>
+            State.modify(_.makeOp(cur, DeleteM))
 
-        // GET
-        case DownField(cur, key) =>
-          cur.finalKey match {
-            case HeadK => State.pure(cur)
-            case _ => State.pure(cur.append(MapT.apply, StrK(key)))
-          }
+          // GET
+          case DownField(cur, key) =>
+            cur.finalKey match {
+              case HeadK => State.pure(cur)
+              case _ => State.pure(cur.append(MapT.apply, StrK(key)))
+            }
 
-        // ITER
-        case Iter(cur) =>
-          State.pure(cur.append(ListT.apply, HeadK))
+          // ITER
+          case Iter(cur) =>
+            State.pure(cur.append(ListT.apply, HeadK))
 
-        // NEXT1
-        case Next(cur) =>
-          State.inspect(_.document.next(cur))
+          // NEXT1
+          case Next(cur) =>
+            State.inspect(_.document.next(cur))
 
-        // KEYS1
-        case Keys(cur) =>
-          State.inspect(_.document.keys(cur))
+          // KEYS1
+          case Keys(cur) =>
+            State.inspect(_.document.keys(cur))
 
-        // VAL1
-        case Values(cur) =>
-          State.inspect(_.document.values(cur))
-      }
-  }
+          // VAL1
+          case Values(cur) =>
+            State.inspect(_.document.values(cur))
+        }
+    }
 }
